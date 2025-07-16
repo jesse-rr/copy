d8e37e344b2d81bc45d468c66d2e4aa2eb242991
jesse-rr <jessericardorogerio@gmail.com>
2025-07-15 21:42:35 -0300

    o

---
+++ bcd/src/main/java/com/example/bcd/repository/DesafioDeEspecialidadeFeitaRepository.java
@@ -0,0 +1,7 @@
+package com.example.bcd.repository;
+
+import com.example.bcd.model.DesafioDeEspecialidadeFeita;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DesafioDeEspecialidadeFeitaRepository extends JpaRepository<DesafioDeEspecialidadeFeita, Long> {
+}
---
+++ bcd/src/main/java/com/example/bcd/repository/EspecialidadeRepository.java
@@ -0,0 +1,7 @@
+package com.example.bcd.repository;
+
+import com.example.bcd.model.Especialidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long> {
+}
